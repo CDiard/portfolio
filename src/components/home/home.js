5ac14deb84d0e524ed8db5620df6e52f6033b417
Meezyr <corentin.d@polarion.fr>
2025-05-31 20:52:23 +0200

    [Feature] Swiper component

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,6 +2,7 @@ import template from './home.html';
 import './home.css';
 
 import '/src/components/svg/svg.js';
+import '/src/components/swiper/swiper.js';
 
 export default function Home() {
     const div = document.createElement('div');
---
+++ src/components/swiper/swiper.js
@@ -0,0 +1,84 @@
+import './swiper.css';
+
+import data from '/src/assets/data/swiper-data.json';
+
+import Swiper from 'swiper';
+import {Navigation} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+class AppSwiper extends HTMLElement {
+    connectedCallback() {
+        this.renderSlides();
+    }
+
+    renderSlides() {
+        this.innerHTML = `
+        <div class="swiper">
+            <div class="swiper-wrapper">
+                ${data.map((item, index) => `
+                    <div class="swiper-slide">
+                        <img src="${item.image}" class="img-fluid" alt="${item.title}" />
+                        <p class="m-0"><strong>${item.title}</strong></p>
+                        <p class="m-0">${item.description}</p>
+                        <button class="btn btn-secondary" data-id="${index}" data-bs-toggle="offcanvas" data-bs-target="#slideOffcanvas" title="En savoir plus sur la réalisation">
+                            En savoir plus
+                            <app-svg icon="arrow_right"></app-svg>
+                        </button>
+                    </div>
+                `).join('')}
+            </div>
+            <div class="swiper-pagination"></div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </div>
+
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="slideOffcanvas">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasTitle">Détails</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Fermer"></button>
+            </div>
+            <div class="offcanvas-body" id="offcanvasContent">
+                Chargement...
+            </div>
+        </div>`;
+
+        requestAnimationFrame(() => {
+            const swiperElement = this.querySelector('.swiper');
+            if (swiperElement) {
+                new Swiper(swiperElement, {
+                    modules: [ Navigation ],
+                    loop: true,
+                    navigation: {
+                        nextEl: this.querySelector('.swiper .swiper-button-next'),
+                        prevEl: this.querySelector('.swiper .swiper-button-prev'),
+                    },
+                    slidesPerView: 3,
+                    spaceBetween: 20,
+                });
+            } else {
+                console.warn('<app-swiper> : élément .swiper introuvable');
+            }
+        });
+
+        this.addEventListener('click', e => {
+            const btn = e.target.closest('button[data-id]');
+            if (btn) {
+                const id = parseInt(btn.getAttribute('data-id'));
+                const slide = data[id];
+                const title = this.querySelector('#offcanvasTitle');
+                const content = this.querySelector('#offcanvasContent');
+                if (slide && title && content) {
+                    title.textContent = slide.title;
+                    content.innerHTML = `
+                        <p><strong>Description :</strong> ${slide.description}</p>
+                        <p><strong>Détails :</strong> ${slide.details}</p>
+                        <img src="${slide.image}" class="img-fluid mt-2" alt="${slide.title}" />
+                    `;
+                }
+            }
+        });
+    }
+}
+
+customElements.define('app-swiper', AppSwiper);
