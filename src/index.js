74d8aff041d2768bcd27c83d50b6000c1e68d70c
Meezyr <corentin.d@polarion.fr>
2025-06-10 19:42:22 +0200

    [Feature] Style, custom fonts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import './assets/styles/main.css';
 import './router';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import './assets/styles/index.css';
 
 import './components/header/header.js';
 import './components/footer/footer.js';
